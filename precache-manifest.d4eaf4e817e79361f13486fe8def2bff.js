93a64c6badea3740822b28cb044d2e5316ef860f
Rishabh Khanna <rishabhrrk@gmail.com>
2021-02-02 18:46:29 -0500

    Updates

---
+++ precache-manifest.d4eaf4e817e79361f13486fe8def2bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a430f168132d1fd8907140206125de62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fadb5c6e2baab0269a0",
+    "url": "/static/css/main.6dbbb12d.chunk.css"
+  },
+  {
+    "revision": "5bd8a08c25b5dc5fddc1",
+    "url": "/static/js/2.5f03bd65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f03bd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fadb5c6e2baab0269a0",
+    "url": "/static/js/main.f311b3d7.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
