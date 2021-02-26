d7241beda3c63292d7c106d782ba05e80915b113
Rishabh Khanna <rishabhrrk@gmail.com>
2021-02-26 05:58:46 -0500

    Changes for new projects authentication service, shorturl, vending machine

--- src/resumeData.js
+++ src/resumeData.js
@@ -18,14 +18,14 @@ let resumeData = {
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/smartrrk",
+          "url":"http://twitter.com/rishabhrrk.",
           "className":"fa fa-twitter"
         }
       ],
     "aboutme":"I am currently a master's student at University of Florida majoring in Computer Sciences. I am a self taught programmer, currently diving deeper into Full-Stack development. If I were to describe my self in a sentence I would say - “I take responsibility for my craft and believe in my abilities to build a great product”.",
     "address":"Gainesville, Florida, US",
-    "website":"https://rishabhrrk.github.io/react/",
-    "email":"rishabhrrk@gmail.com",
+    "website":"https://rishabhrrk.github.io/",
+    "email":"rishabh.khanna@ufl.edu",
     "mobile":"+1 352 281 9920",
     "education":[
       {
@@ -33,31 +33,31 @@ let resumeData = {
         "specialization":"MS Computer Science",
         "GPA":"3.67/4",
         "MonthOfStarting":"August",
-        "YearOFStarting":"2019",
+        "YearOfStarting":"2019",
         "MonthOfPassing":"May",
         "YearOfPassing":"2021",
-        "Achievements":"Coursework – Algorithms, Advanced Data Structures, Distributed Systems, Computer Networks, Machine Learning"
+        "Achievements":"Coursework – Algorithms, Advanced Data Structures, Distributed Systems, Computer Networks, Machine Learning, Data Science"
       },
       {
         "UniversityName":"GGSIPU",
         "specialization":"B.Tech. Computer Science",
         "GPA":"73.1/100",
         "MonthOfStarting":"August",
-        "YearOFStarting":"2012",
+        "YearOfStarting":"2012",
         "MonthOfPassing":"May",
         "YearOfPassing":"2016",
-        "Achievements":"Coursework – Algorithms, Operating Systems, OOP using Java, Software Engineering, Database Managaement Systems, Data Warehousing & Minin"
+        "Achievements":"Coursework – Algorithms, Operating Systems, OOP using Java, Software Engineering, Database Managaement Systems, Data Warehousing & Mining"
       }
     ],
     "work":[
       {
         "CompanyName":"University of Florida",
         "specialization":"Full Stack Engineer",
-        "MonthOfLeaving":"August",
+        "MonthOfLeaving":"July",
         "YearOfLeaving":"2020",
         "MonthOfJoining":"October",
         "YearOfJoining":"2019",
-        "Achievements":"• Developed an agro-app to predict the nitrogen rate appropriate for potato cultivation in Maine.        • Developed a linear regression model, using Pandas and NumPy, to predict N rate and achieved accuracy of more than 90%.        • APIs were created using Nodejs and PostgreSQL was employed as database. Successfully deployed the API on Heroku cloud.        • Designed the front end of the web portal using React, JavaScript and API calls were made using Axios.        • Developed android app using React Native, JavaScript and Axios was used to make API calls. "
+        "Achievements":"• Developed mobile and web applications for the Soil and Water Sciences Department at the University of Florida to predict the optimal nitrogen rate for sustainable potato cultivation.        • Built the RESTful back-end service with NodeJS on a PostgreSQL database, the web client with React, HTML 5, and CSS 3.0, and the mobile client with React-Native.        • Designed and optimized a linear regression model to achieve a test accuracy of 92%; deployed and managed the service for ~1000 clients on a Heroku instance."
       },
       {
         "CompanyName":"O9 Solutions",
@@ -66,7 +66,7 @@ let resumeData = {
         "YearOfLeaving":"2019",
         "MonthOfJoining":"May",
         "YearOfJoining":"2018",
-        "Achievements":"•	Created Python and C# scripts for interacting with various platform APIs to pull data from or push data to the O9 tool.        •	Built backends of utilities and applications for consultants and clients to support their day-to-day work using Python.        •	Implemented an Extract Transform Load (ETL) solution for Navico (client) using SSIS and SQL Server 2012.        •	Improved SQL code’s time efficiency by 50% for IB (client) by optimizing joins and using techniques like indexing and TEMP tables.        •	Used Apache Spark and Scala to perform data cleaning and ETL as part of a self-initiated Proof of Concept (PoC).        "
+        "Achievements":"• Developed applications for two Fortune 500 clients to automate supply chain workflows using C#, Java, and Python.        • Built the data pipeline (modeled data, designed schema, and implemented an Extract Transform Load (ETL) solution) for Navico (client) to visualize Supply plans and forecast demand using SQL Server Integration Service; optimized the pipeline by incorporating real-time stream processing using Spark and Storm, reducing processing time by 30%.        • Improved SQL batch’s time efficiency by 50% for IB (client) by optimizing joins and using indexing and TEMP tables."
       },
       {
         "CompanyName":"Tata Consultancy Services",
@@ -75,7 +75,7 @@ let resumeData = {
         "YearOfLeaving":"2018",
         "MonthOfJoining":"October",
         "YearOfJoining":"2016",
-        "Achievements":"•	Automated various web applications, Windows applications and workflows using .NET and C#, Python and batch scripting as part of Robo-Process Automation team which benefitted various projects by reducing timelines and expenses.        •	Contributed to a ticketing application for customer care team. Worked on an escalation microservice using MVC framework in C#, ASP .NET, and database as SQL Server.        •	Developed PL/SQL procedures and functions for optimizing inventory of ABB (client) as part of Supply Chain Management IBP implementation. Optimization caused reduction in expenses by $1 Million across 4 locations.        •	Tested and improved performance of existing PL/SQL procedures by achieving reduction in running time by 30%.        •	Lead a team of 3 developers and built a PoC using Hadoop ecosystem – Sqoop, Hive and Java. Achieved improvement in performance in contrast to existing SQL implementation. Solution was deployed on 5 AWS EC2 nodes using Cloudera Manager.         "
+        "Achievements":"• Developed PL/SQL procedures for optimizing inventory of ABB (client), reducing expenses by $1 Million annually.        • Lead a pilot project with a team of 3 developers to reduce time for data processing using Hadoop and Hive; deployed it on AWS EC2 using Cloudera Manager; achieved performance improvement of 23% over the existing SQL implementation.        • Automated workflows using C#, Java, Python and Bash for in-house projects, reducing project budgets by 10% overall.        • Built our ticketing application’s escalation service using the service-oriented architecture on ASP.NET MVC 5 framework."
       }
     ],
     "skillsDescription":[
@@ -83,13 +83,13 @@ let resumeData = {
         "Programming Languages : Python, C#, Java, Scala, Elixir, JavaScript"
       ,
       
-        "Frameworks : React, Nodejs, React Native, .NET, Apache Spark, Hadoop"
+        "Frameworks : React, Nodejs, Express, React Native, .NET, Apache Spark, Hadoop"
       ,
       
-        "Databases : SQL Server, MongoDB, PostgreSQL, MySQL, Oracle"
+        "Databases : SQL Server, MongoDB, PostgreSQL, MySQL, Oracle, Redis, NoSQL"
       ,
       
-        "Others : SQL, T-SQL, PL/SQL, SSIS, Batch Scripting, R, HTML, CSS"
+        "Others : SQL, T-SQL, PL/SQL, SSIS, Batch Scripting, R, HTML, CSS, PyTorch"
       
     ],
     "skills":[
@@ -97,16 +97,44 @@ let resumeData = {
         "Programming Languages":"Python, C#, Java, Scala, Elixir, JavaScript"
       },
       {
-        "Frameworks":"React, Nodejs, React Native, .NET, Apache Spark, Hadoop"
+        "Frameworks" : "React, Nodejs, Express, React Native, .NET, Apache Spark, Hadoop"
       },
       {
-        "Databases":"SQL Server, MongoDB, PostgreSQL, MySQL, Oracle"
+        "Databases":"SQL Server, MongoDB, PostgreSQL, MySQL, Oracle, Redis, NoSQL"
       },
       {
-        "Others":"SQL, T-SQL, PL/SQL, SSIS, Batch Scripting, R, HTML, CSS"
+        "Others":"SQL, T-SQL, PL/SQL, SSIS, Batch Scripting, R, HTML, CSS, PyTorch"
       }
     ],
     "portfolio":[
+      {
+        "name":"Vending Machine",
+        "description":"Developed a full stack web portal for online purchase of sodas. The backend is built in RESTful architecture on NodeJS. The front end is built using React, HTML, CSS and Bootstrap 4. Front end provides UI for customers as well as admin staff for manipulating the inventory; like changing prices, changing stocks and adding or deleting sodas. Application can be configured to be connected with different databases or file systems.",
+        "technologies":["NodeJS", "React", "CSS", "HTML", "Bootstrap 4"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/vending_machine/"
+      },
+      {
+        "name":"Authentication Service",
+        "description":"Built a middleware module to provide JWT (JSON Web Token) based user-authentication. Designed the RESTful API using NodeJS and Express on a Redis and MySQL backend.",
+        "technologies":["NodeJS", "Express", "JWT", "BCrypt"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/dbadmin/"
+      },
+      {
+        "name":"Short URL",
+        "description":"Built a service to shorten a URL.",
+        "technologies":["NodeJS", "Express", "React"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/shorturl"
+      },
+      {
+        "name":"Search a Million Names",
+        "description":"A service which searches a million names and return suggestions based on the current state of search.",
+        "technologies":["NodeJS", "Express", "React", "MySQL"],
+        "imgurl":"images/portfolio/phone.jpg",
+        "url":"https://github.com/rishabhrrk/searchresultbackend"
+      },
       {
         "name":"Click Counter",
         "description":"Application made it possible to search iTunes for different kind of content and kept track of user’s click on each content. The Clicks were used for analyzing user trends and geographic trends of content.	Built backend on C#, ASP .NET using MVC framework and front end was designed using MaterializeCSS, CSS, and HTML. JavaScript, jQuery and AJAX were used for passing data between views.",
@@ -122,9 +150,9 @@ let resumeData = {
         "url":"https://rishabhrrk.github.io/nrate/"
       },
       {
-        "name":"Budiary",
-        "description":"The application keeps track of budgets and expenses.",
-        "technologies":["Nodejs", "Express", "MongoDB"],
+        "name":"Product Review Search Engine",
+        "description":"Developed a search application for product reviews and ratings in a product store; hosted the front end and JSON datastore on AWS S3, utilizing AWS Lambda for application logic.",
+        "technologies":["AWS", "S3", "Step Function", "Lambda", "SNS"],
         "imgurl":"images/portfolio/project.jpg"
       },
       {
